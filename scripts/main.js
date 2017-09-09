0a0eae74e4d26a85dc41e1c925a30d1e9a7ff0b0
Simone Duca <simone@zooniverse.org>
2017-09-09 23:24:43 +0100

    Port app

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+
+$(function() {
+
+    // Populate using an object literal
+    // WTF.init({ your: Object });
+
+    // Populate using a JSON file
+    WTF.init( 'zooniverse.json' );
+
+    // Populate using a Google spreadsheet ID (you must publish it first!)
+    // WTF.init( 'yourGoogleSpreadsheetID' );
+
+});
