9de5403b3e63204b1c54baf3b28fd503e7315462
Simone Duca <simone@zooniverse.org>
2017-09-12 15:14:08 +0100

    Add ellipsis

--- scripts/wtf.js
+++ scripts/wtf.js
@@ -34,7 +34,7 @@ var WTF = (function() {
     */
 
     function start() {
-        
+
         // Copy out templates then remove from corpus
 
         templates = corpus.template;
@@ -129,16 +129,16 @@ var WTF = (function() {
         var i, n, key, val, map = {}, keys = {}, data = {}, rows = json.feed.entry;
 
         for ( key in rows[0] ) {
-            
+
             if ( RE_COL.test( key ) ) {
-                
+
                 map[ key ] = key.match( RE_COL )[ 1 ].toLowerCase();
                 keys[ key ] = [];
             }
         }
 
         for ( key in keys ) {
-            
+
             data[ map[ key ] ] = keys[ key ];
 
             for ( i = 0, n = rows.length; i < n; i++ ) {
@@ -200,7 +200,7 @@ var WTF = (function() {
             }
             return a.length > b.length ? -1 : 1
         })
-        
+
         var content = '@(type)'.replace( 'type', types.join( '|' ) );
 
         regex = new RegExp( content, 'gi' );
@@ -238,7 +238,7 @@ var WTF = (function() {
         dom.generate.text( randomItem( responses ) );
         dom.output.html(
             '<dl>' +
-                '<dt>' + randomItem( headings ) + '</dt>' +
+                '<dt>' + randomItem( headings ) + '&hellip;</dt>' +
                 '<dd>' + idea + '</dd>' +
             '</dl>'
         );
@@ -278,7 +278,7 @@ var WTF = (function() {
         for ( var key in corpus )
 
             copy[ key ] = corpus[ key ].concat();
-        
+
         return copy;
     }
 
@@ -297,7 +297,7 @@ var WTF = (function() {
             Expects one of the following:
 
                 1.  An object with `templates` and any amount of keys for word types, for example:
-        
+
                     {
                         templates: [ 'The @color @animal', 'The @animal was @color' ],
                         animal: [ 'dog', 'cat', 'rabbit' ],
@@ -307,7 +307,7 @@ var WTF = (function() {
                 2.  A path to a JSON file with the same structure as above (see `sample.json`)
 
                 3.  A Google spreadsheet key (e.g 0AvG1Hx204EyydF9ub1M2cVJ3Z1VGdDhTSWg0ZV9LNGc)
-                    You must first publish the spreadsheet as a CSV 
+                    You must first publish the spreadsheet as a CSV
                     @see https://support.google.com/drive/answer/37579?hl=en
 
         */
